9c8fcc2f5bb0737b6776e3eb1f66d80713d8cd5c
raahimk <87918061+raahimk@users.noreply.github.com>
2022-10-15 10:06:38 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABKnpumgpDKlEDlESJnp4ZgSJl2Vfi9yM",
+    authDomain: "project-40-aaecf.firebaseapp.com",
+    databaseURL: "https://project-40-aaecf-default-rtdb.firebaseio.com",
+    projectId: "project-40-aaecf",
+    storageBucket: "project-40-aaecf.appspot.com",
+    messagingSenderId: "264800013934",
+    appId: "1:264800013934:web:bc20324a7453bcde79207f"
+  };
